e36ac172801b940da77bc4b8b50d713b370fca36
Liuxianqin <ruosuiwanfeng@gmail.com>
2019-10-20 17:37:06 +0800

    linkedlist

---
+++ DS-learning/LinkedList/src/LinkedList.java
@@ -0,0 +1,141 @@
+import java.util.SplittableRandom;
+
+public class LinkedList<E> {
+
+    private class Node{
+        public E e;
+        public Node next;
+
+        public Node(E e,Node next){
+            this.e =e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this(e,null);
+        }
+
+        public Node(){
+            this(null,null);
+        }
+
+        @Override
+        public String toString(){
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int  size=0;
+
+    public LinkedList(){
+        dummyHead = new Node(null,null);
+        size = 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size ==0;
+    }
+
+
+
+
+    //在链表中间添加元素
+    public void add(int index , E e){
+        if(index < 0 || index > size)
+            throw new IllegalArgumentException("Illegal index");
+
+        Node prev = dummyHead;
+        for(int i = 0; i <index;i++)
+            prev = prev.next;
+//            Node node = new Node(e);
+//            node.next = prev.next;
+//            prev.next = node;
+      //  prev = new Node(e,prev);                     //为什么昨天写成了这样
+        prev.next = new Node(e,prev.next);
+        size ++;
+
+    }
+
+    //在链头部添加元素e
+    public void addFirst(E e){
+        add(0,e);
+    }
+    //在链表末尾添加元素
+    public void addLast(E e){
+        add(size,e);
+    }
+
+    //联系使用 不经常用到
+    public E get(int index){
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Illegal index");
+        Node cur = dummyHead.next;  //从索引为0的元素开始
+        for(int i=0;i<index;i++)
+            cur = cur.next;
+        return cur.e;
+    }
+    public E getFirst(){
+        return get(0);
+    }
+    public E getLast(){
+        return get(size-1);
+    }
+
+    public void set(int index,E e){
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Illegal index");
+        Node cur = dummyHead.next;
+        for(int i=0;i<index ;i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    public boolean contains(E e){
+
+
+        Node cur = dummyHead.next;
+        while(cur != null){
+            if(cur.e.equals(e))
+                return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public E remove(int index){
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Illegal index");
+        Node prev = dummyHead.next;
+        for(int i=0;i<index-1;i++)
+            prev = prev.next;
+        Node delNode = prev.next;
+        prev.next = delNode.next;
+        delNode.next = null;
+        return delNode.e;
+    }
+
+    public E removeFirst(){
+        return remove(0);
+    }
+    public E removeLast(){
+        return remove(size-1);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while(cur != null){
+            res.append(cur +" -> ");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+}
+
+
